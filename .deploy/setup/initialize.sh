b5690263b8d9ba88ad7fcbb23923217ab126c50e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-01-03 11:01:41 -0700

    chore(docker): update definition to use vtk-egl

--- .deploy/setup/initialize.sh
+++ .deploy/setup/initialize.sh
@@ -1,3 +1,5 @@
 # Install app from local directory
 pip install wheel # needed to install /local-app
 pip install /local-app
+pip uninstall -y vtk
+pip install --extra-index-url https://wheels.vtk.org vtk-egl
